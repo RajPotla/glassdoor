375383afdd71a851fc2823f2b1581835ea15c791
RajPotla <33848587+RajPotla@users.noreply.github.com>
2020-06-22 18:35:39 -0500

    Update ReviewManagementController.cs
    Needs Testing (No vs file included in repo to test)

--- Controllers/ReviewManagementController.cs
+++ Controllers/ReviewManagementController.cs
@@ -13,5 +13,59 @@ namespace Glassdoor.Controllers
         {
             return View();
         }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Title,Description,Date_Created, User_ID,Job_ID")] ReviewManagementController Review)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Review.id = Guid.NewGuid();
+                    Review.Date_Created = DateTime.Today;
+                    db.Review.Add(review);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            catch (InvalidDataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes, Try again!");
+            }
+
+            return View(Review);
+        }
+        
+        // POST: Sellers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(Guid? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ReviewManagementController reviewToUpdate = db.ReviewManagementController.Find(id);
+            if(TryUpdateModel(reviewToUpdate, "", new string[] { "Description", "Date_Created", "User_ID", "Job_ID" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (InvalidDataException)
+                {
+                    ModelState.AddModelError("", "Unable to save Changes, Try Again!");
+                }
+            }
+
+            return View(reviewToUpdate);
+        }
+        
     }
 }
